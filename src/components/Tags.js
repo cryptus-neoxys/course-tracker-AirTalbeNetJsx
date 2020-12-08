6d470e222f513f88db8f96d47eb834d78e69c459
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-09 03:16:08 +0530

    Tags component

---
+++ src/components/Tags.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+export default function Tags({ tagsUpdated, idx }) {
+  const tagChoices = [
+    "React",
+    "Node",
+    "Express",
+    "NEXT",
+    "AWS",
+    "Serverless",
+    "JAMStack",
+  ];
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  useEffect(() => {
+    setSelectedTags([]);
+  }, [idx]);
+
+  const tagChange = (e) => {
+    const value = e.target.value;
+    const alreadySelected = selectedTags.includes(value);
+    if (e.target.checked && !alreadySelected) {
+      setSelectedTags([...selectedTags, value]);
+    } else if (!e.target.checked && alreadySelected) {
+      setSelectedTags(selectedTags.filter((prevTag) => prevTag !== value));
+    }
+  };
+
+  useEffect(() => {
+    tagsUpdated(selectedTags);
+  }, [selectedTags, tagsUpdated]);
+
+  return (
+    <>
+      {tagChoices.map((choice, index) => (
+        <label className="checkbox-inline mr-3" key={index}>
+          <input type="checkbox" value={choice} onChange={tagChange} />
+          {" " + choice}
+        </label>
+      ))}
+    </>
+  );
+}
