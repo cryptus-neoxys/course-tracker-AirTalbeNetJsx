ce69e2d431df2fcdf42f3852fba8592df3536510
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-09 03:06:44 +0530

    AirTable Config

---
+++ functions/helpers/airtable.js
@@ -0,0 +1,8 @@
+// TODO: create Airtable configuration
+require("dotenv").config();
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+module.exports = { table };
